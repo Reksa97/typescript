13c5070fea0146092492346bf4eff8a1a1fa4e30
Reko <reko.kalkaja@gmail.com>
2022-11-30 19:31:36 +0200

    9.15 done

--- react-ts-app/src/App.tsx
+++ react-ts-app/src/App.tsx
@@ -1,43 +1,142 @@
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
+  type: string;
+}
+
+interface CoursePartBaseWithDesc extends CoursePartBase {
+  description: string;
+}
+
+interface CourseNormalPart extends CoursePartBaseWithDesc {
+  type: "normal";
+}
+
+interface CourseProjectPart extends CoursePartBase {
+  type: "groupProject";
+  groupProjectCount: number;
+}
+
+interface CourseSubmissionPart extends CoursePartBaseWithDesc {
+  type: "submission";
+  exerciseSubmissionLink: string;
+}
+
+interface CourseBackendPart extends CoursePartBaseWithDesc {
+  type: "special";
+  requirements: string[];
+}
+
+type CoursePart =
+  | CourseNormalPart
+  | CourseProjectPart
+  | CourseSubmissionPart
+  | CourseBackendPart;
+
+const courseParts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is the easy course part",
+    type: "normal",
+  },
+  {
+    name: "Advanced",
+    exerciseCount: 7,
+    description: "This is the hard course part",
+    type: "normal",
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    type: "groupProject",
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+    type: "submission",
+  },
+  {
+    name: "Backend development",
+    exerciseCount: 21,
+    description: "Typing the backend",
+    requirements: ["nodejs", "express", "jest"],
+    type: "special",
+  },
+];
+
 const Header = ({ courseName }: { courseName: string }) => (
   <h1>{courseName}</h1>
 );
 
-interface CoursePart {
-  name: string;
-  exerciseCount: number;
-}
+const Part = ({ p }: { p: CoursePart }) => {
+  const PartHeader = (
+    <b>
+      {p.name} {p.exerciseCount}
+    </b>
+  );
+  switch (p.type) {
+    case "normal":
+      return (
+        <p>
+          {PartHeader}
+          <br />
+          <i>{p.description}</i>
+        </p>
+      );
+    case "groupProject":
+      return (
+        <p>
+          <b>
+            {p.name} {p.exerciseCount}
+          </b>
+          <br />
+          There are {p.groupProjectCount} group projects in this part
+        </p>
+      );
+    case "submission":
+      return (
+        <p>
+          {PartHeader}
+          <br />
+          <i>{p.description}</i>
+        </p>
+      );
+    case "special":
+      return (
+        <p>
+          {PartHeader}
+          <br />
+          <i>{p.description}</i>
+          <br />
+          Required skills:
+          {p.requirements.map((r, i, a) =>
+            i + 1 === a.length ? ` ${r}` : ` ${r},`
+          )}
+        </p>
+      );
+    default:
+      return null;
+  }
+};
 
 const Content = ({ courseParts }: { courseParts: CoursePart[] }) => (
   <>
     {courseParts.map((p) => (
-      <p key={p.name}>
-        {p.name} {p.exerciseCount}
-      </p>
+      <Part key={p.name} p={p} />
     ))}
   </>
 );
 
 const Total = ({ numberOfExercises }: { numberOfExercises: number }) => (
-  <p>Number of exercises {numberOfExercises}</p>
+  <p>Number of exercises in the whole course: {numberOfExercises}</p>
 );
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts: CoursePart[] = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7,
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14,
-    },
-  ];
-
   return (
     <div>
       <Header courseName={courseName} />
